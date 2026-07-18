548854a5c335709e84cb8e6e5709425b00e3ea3f
Emmanuel Mtz <hemmanuelmtz777@gmail.com>
2026-07-18 09:21:26 -0600

    proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
         'admin.api' => \App\Http\Middleware\AdminApiMiddleware::class,
     ]);
+    $middleware->trustProxies(at:'*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
